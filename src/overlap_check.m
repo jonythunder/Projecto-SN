e4308e5a4386c24acb90be3ad127ce208881d566
Joao Goncalo Filipe Manito <joaomanito@Maki.local>
2018-06-04 18:52:28 +0100

    Adicionada a definição do cilindro e a verificação de colisão com o mesmo, com slices

--- src/overlap_check.m
+++ src/overlap_check.m
@@ -5,9 +5,10 @@ function [ifoverlap, overlap_areas] = overlap_check(xyz)
 	const.a = 6378137;
 	const.f = 1/298.257223563;
 	overlap_areas = [];
-	ifoverlap = 0;
+	ifoverlap = false;
 	
 	% -- Definition of restricted areas
+	% Sphere
 	area1.id = 1;
 	area1.type = "sphere"; % -- Types: sphere, cylinder, box
 	area1.radius = 20; % -- Meters
@@ -15,11 +16,41 @@ function [ifoverlap, overlap_areas] = overlap_check(xyz)
 								dirchk(dms2rad([9,08,18.91]),'W')...
 								102],const.a,const.f); % -- xyz, meters
 
+	% Cylinder
+	area2.id = 2;
+	area2.type = "cylinder"; % -- Types: sphere, cylinder, box
+	area2.radius = 20; % -- Meters
+	area2.height = 20; % -- Meters
+	area2.bottom_xyz = llh2xyz([	dirchk(dms2rad([38,44,12.46]),'N')...
+								dirchk(dms2rad([9,08,18.91]),'W')...
+								102],const.a,const.f); % -- xyz, meters
+	area2.bottom_llh = ([	dirchk(dms2rad([38,44,12.46]),'N')...
+								dirchk(dms2rad([9,08,18.91]),'W')...
+								102],const.a,const.f); % -- xyz, meters
+
+
+
 	% -- Check intrusion
 	if area1.type == "sphere"
 		dist = norm(xyz - area1.center);
-		if dist < area1.radius
-			ifoverlap = 1; overlap_areas = [overlap_areas area1.id];
+		if dist <= area1.radius
+			ifoverlap = true; overlap_areas = [overlap_areas area1.id];
+		end
+	end
+
+	if area2.type == "cylinder"
+		%Fazer a divisão do cilindro em fatias, e usar a altitude do receptor móvel para 
+		%determinar a intrusão
+		% -- Converter para LLH para obter a altitude -- %
+		[llh]=xyz2llh(xyz,const.a,const.f);
+		if llh(3)<=area2.height
+			%Define the slice height
+			slice_center=([area2.bottom_llh(1);area2.bottom_llh(2);llh(3)]);
+			slice_center_xyz=llh2xyz(slice_center,const.a,const.f);
+			dist = norm(xyz - slice_center_xyz);
+			if dist <= area2.radius
+				ifoverlap = true; overlap_areas = [overlap_areas area2.id];
+			end
 		end
 	end
 end

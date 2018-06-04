08bc60727dde6ed859f0efede3ab613c43bf2b22
ekspek <daniel.de.schiffart@gmail.com>
2018-06-04 17:29:02 +0100

    Sketched final functions for DGPS calculations, updated README

---
+++ src/gs_error.m
@@ -0,0 +1,6 @@
+% Função que recebe os pseudo-ranges da ground station gs, cuja posição é
+% conhecida, e as efemérides dos sateélites, válidas para o momento em
+% causa. Devolve o erro do pesudo-range para cada satélite.
+function satellites_prerror = gs_error(fd_eph,fd_gspr)
+
+end
--- src/main.m
+++ src/main.m
@@ -1,3 +1,4 @@
+%% Startup
 addpath('./generic_functions/');
 warning('off','backtrace');
 
@@ -5,20 +6,34 @@ const.a = 6378137;
 const.f = 1/298.257223563;
 const.c = 299792458;
 
-%Este ficheiro tem como objectivo utilizar dados NMEA para validar o
-%algoritmo de detecção de intrusão.
+% fd_eph = fopen('ephemerides.eph'); % -- Verificao de presenca de ficheiro
+% if fd_eph == -1
+% 	error('File "ephemerides.eph" not found.');
+% end
+% 
+% fd_gspr = fopen('pseudoranges.pr'); % -- Verificao de presenca de ficheiro
+% if fd_gspr == -1
+% 	error('File "presudoranges.pr" not found.');
+% end
 
-%load input data
-input_data_path = 'test_data/teste2.nmea';
+input_data_path = 'test_data/teste2.nmea'; % -- load input data
 fin = fopen(input_data_path);
 if fin == -1
     disp('Erro no ficheiro');
     return;
 end
 
+% Calcular os erros do pseudo-range com base na estação local
+% satellites_prerror = gs_error;
+
 %parse the NMEA data and get a vector of (Lat,Lon,Height,time) points
 [position_points] = parse_NMEA(fin);
 
+%% Position based on pseudo-ranges
+
+% xyz = gnsspos_final(pr_current, satellites_prerror);
+
+%% Overlap check
 count = 0; triggered_lines = [];
 for aux = 1:max(size(position_points))
 	point = llh2xyz([deg2rad(position_points(aux,1:2)) position_points(aux,3)],const.a,const.f);

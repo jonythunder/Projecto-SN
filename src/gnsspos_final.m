511d8e59b4ee4b01cbca77205be24d6b0ce00daf
htaam <39907025+htaam@users.noreply.github.com>
2018-06-05 18:20:49 +0100

    Added a function to calculate the positions of a satalite at time of transmition for a specific time received.

--- src/gnsspos_final.m
+++ src/gnsspos_final.m
@@ -1,6 +1,6 @@
 % Função que calcula a posição actual utilizando DGPS, recebendo os
 % pseudo-ranges obtidos pelo recetor GPS e os erros calculados para cada
 % satélite pela estação terrestre.
-function xyz = gnsspos_final(pr_current, satellites_prerror)
+function xyz = gnsspos_final(last_pos,sat_pos,pr_current, satellites_prerrors)
 	
 end

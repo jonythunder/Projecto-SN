171a90d80af30b59f9ae0464236f23803adbd60a
htaam <39907025+htaam@users.noreply.github.com>
2018-06-13 13:53:08 +0100

    Corected some of the time inputs that were not multiplied by const.c We are now 300Km below the surface of marrocos. Adding troposphere takes us to outer space.

--- src/satellite_positions.m
+++ src/satellite_positions.m
@@ -101,8 +101,8 @@ function [satellites_pos] = satellite_positions(input_eph,input_hui,WN,TOW,ref)
         tropo = troposphere_model(ref,s,WN,TOW);
         iono = ionosphere_model(alpha,beta,ref,s,t_tx);
         t1 =(const.F*(e^(input_eph(aux,34)))*sin(E));
-        t2 = input_eph(aux,18);
-		t3 = input_hui(1,13)+(input_hui(1,15)*(t_tx-input_eph(aux,22)))+(input_hui(1,17)*((t_tx-input_eph(aux,22))^(2)));
+        t2 = input_eph(aux,18)*const.c;
+		t3 = (input_hui(1,13)+(input_hui(1,15)*(t_tx-input_eph(aux,22)))+(input_hui(1,17)*((t_tx-input_eph(aux,22))^(2))))*const.c;
         
         delta_tsv = t1-t2+t3-iono;%-tropo;
     
